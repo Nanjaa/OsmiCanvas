02bf6f725d43094de89d09469969aad65e1c66d3
Stephanie Olfert <steph.o-91@hotmail.com>
2015-04-18 09:50:35 -0500

    Deleted buttons I didn't want, fixed the 'add squares' function, and deleted borders and console.logs

--- generator.js
+++ generator.js
@@ -1,156 +1,192 @@
 $(document).ready(function() {
-	var bordersShown = true;
-	document.oncontextmenu = function() {return false;};
-	var mouseDown = false;
-	$('.faq').hide();
-	$('.dropDownContent').hide();
-	var dropDownStatus = false;
 
-// repeating the squares 
+// here is where the js will begin
 
-	var squaresArray = new Array();
-	for(var i=0; i<3000; i++) {
-		squaresArray.push('<div class = "square"></div>');
-		$('.squareContainer').append(squaresArray[i]);
-	}
+$('.reset').hide();
+$('.resizeContent').hide();
+$('.placementContent').hide();
+$('.faq').hide();
+var bordersShown = true;
+document.oncontextmenu = function() {return false;};
+var mouseDown = false;
+var resizeStatus = false;
+var placementStatus = false;
 
-// coloring the squares 
-	$('.square').click(function() {
-		var newColor = "#" + myColor.value;
-		$(this).css('background-color', (newColor));
-	});
+// NAV BAR!! ----------------------------------------------------
 
-// dragging color
-
-	var clicking = false;
-	$('.square').mousedown(function() {
-		clicking = true;
-		var newColor = "#" + myColor.value;
-		$(this).css('background-color', (newColor));
-	});
-
-	$(document).mouseup(function() {
-		clicking = false;
-	})
-		// mouse click + moving logic here:
-	$('.square').mousemove(function() {
-		if(clicking == false) return;
-		var newColor = "#" + myColor.value;
-		$(this).css('background-color', (newColor));
-
-	});
+// top half -------------------
 
 // setting the background
 
-	$('.backgroundButton').click(function() {
-		$('.pic').attr("src", userInput.value);
-	});
+$('.backgroundButton').click(function() {
+	$('.pic').attr('src', userInput.value);
+});
 
-// toggling the background
+// THE SAVE BUTTON FUNCTION WILL GO HERE!!!!!!!!!!!!!!
 
-	$('.toggleBackgroundButton').click(function() {
-		$('.pic').toggle();
-	})
+// faq
 
-// right click function
-
-	$('.square').mousedown(function(e) {
-		if(e.button == 2) {
-			$(this).css('background-color', 'transparent')
-			return false;
-		}
-		return true;
-	});
-	
-// toggle borders 
-
-	$('.bordersButton').click(function() {
-		if(bordersShown) {
-			$('.square').css('border-color', 'transparent');
-			bordersShown = false;
-		}
-		else {
-			$('.square').css('border-color', 'black');
-			bordersShown = true;
-		};
-	});
-
-// small grid
-
-	$('.small').click(function() {
-		$('.square').css('height', '10px');
-		$('.square').css('width', '10px');
-
-	});
-
-// medium grid
-
-	$('.medium').click(function() {
-		$('.square').css('height', '20px');
-		$('.square').css('width', '20px');
-	});
-
-// large grid
-
-	$('.large').click(function() {
-		$('.square').css('height', '35px');
-		$('.square').css('width', '35px');
-	});
-
-// FAQ
-
-	$('.faqButton').click(function() {
-		$('.gridBody').toggle();
-		$('.faq').toggle();
+$('.faqButton').click(function() {
+	$('.gridBody').toggle();
+	$('.faq').toggle();
 	// toggle the button text
-		var $this = $(this);
-		$this.toggleClass('faqButton');
-		if($this.hasClass('faqButton')) {
-			$this.text('FAQ');
-		}
-		else {
-			$this.text('Return');
-			$this.css('margin-top', '-30px');
-			$this.css('margin-left', '310px');
-	
-		}
-	});
-
-
-// grid resize
-
-	$('.dropDown').click(function() {
-		if(dropDownStatus == false) {
-			$('.dropDownContent').slideDown();
-			dropDownStatus = true;
-			return true;
-		}
-		else {
-			$('.dropDownContent').slideUp();
-			dropDownStatus = false;
-		};
-	});
-
-// picture placement
-
-	$('.placement').click(function() {
-		if(dropDownStatus == false) {
-			$('.placementContent').slideDown();
-			dropDownStatus = true;
-			return true;
-		}
-		else {
-			$('.placementContent').slideUp();
-			dropDownStatus = false;
-		};
-	});
+	var $this = $(this);
+	$this.toggleClass('faqButton');
+	if($this.hasClass('faqButton')) {
+		$this.text('FAQ');
+	}
+	else {
+		$this.text('Return');
+	}
+});
 
 // reset
 
-	$('.resetButton').click(function() {
-		
+$('.resetButton').click(function() {
+	$('.reset').show();
+	$('.yesReset').click(function() {
+		$('.reset').hide();
 		$('.square').css('background-color', 'transparent');
 	})
-
+	$('.noReset').click(function() {
+		$('.reset').hide();
+	});
 });
 
+// bottom half -----------------
+
+// resize
+
+$('.resizeButton').click(function() {
+	if(resizeStatus == false) {
+		$('.resizeContent').slideDown();
+		resizeStatus = true;
+	}
+	else {
+		$('.resizeContent').slideUp();
+		resizeStatus = false;
+	};
+});
+
+// small grid
+
+$('.small').click(function() {
+	$('.square').css('height', '10px');
+	$('.square').css('width', '10px');
+	$('.resizeContent').slideUp();
+	resizeStatus = false;
+});
+
+// medium grid
+
+$('.medium').click(function() {
+	$('.square').css('height', '20px');
+	$('.square').css('width', '20px');
+		$('.resizeContent').slideUp();
+		resizeStatus = false;
+});
+
+// large grid
+
+$('.large').click(function() {
+	$('.square').css('height', '35px');
+	$('.square').css('width', '35px');
+		$('.resizeContent').slideUp();
+		resizeStatus = false;
+});
+
+// toggle background
+
+$('.toggleBackgroundButton').click(function() {
+	$('.pic').toggle();
+})
+
+// toggle grid 
+
+$('.bordersButton').click(function() {
+	if(bordersShown) {
+		$('.square').css('border-color', 'transparent');
+		bordersShown = false;
+	}
+	else {
+		$('.square').css('border-color', 'black');
+		bordersShown = true;
+	};
+});
+
+// GRID BODY ---------------------------------------------------------------
+
+// repeating the squares
+
+var squaresArrayAuto = new Array();
+for(var i=0; i<4000; i++) {
+	squaresArrayAuto.push('<div class="square"></div>');
+	$('.squareContainer').append(squaresArrayAuto[i]);
+};
+
+// coloring the squares
+
+$('.square').click(function() {
+	var newColor = "#" + myColor.value;
+	$(this).css('background-color', (newColor));
+});
+
+// dragging color
+
+var clicking = false;
+$('.square').mousedown(function() {
+	clicking = true;
+	var newColor = "#" + myColor.value;
+	$(this).css('background-color', (newColor));
+})
+
+$(document).mouseup(function() {
+	clicking = false;
+})
+// MOUSE CLICK + MOVING LOGIC HERE: 
+$('.square').mousemove(function() {
+	if(clicking == false) return;
+	var newColor = "#" + myColor.value;
+	$(this).css('background-color', (newColor));
+});
+
+// right click
+
+$('.square').mousedown(function(e) {
+	if(e.button == 2) {
+		$(this).css('background-color', 'transparent')
+		return false;
+	}
+	return true;
+});
+
+// setting the background
+
+$('.backgroundButton').click(function() {
+	$('.pic').attr("src", userInput.value);
+});
+
+// adding more
+
+$('.addSquaresButton').click(function() {
+	var squaresArrayPlus = new Array();
+	for(var i=0; i<addSquaresValue; i++) {
+		squaresArrayPlus.push('<div class="square"></div>');
+		$('.squareContainer').append(squaresArrayPlus[i]);
+	};
+	if($('.square').width() == 10) {
+		$('.square').css('height', '10px');
+		$('.square').css('width', '10px');
+	}
+	else if($('.square').width() == 20) {
+		$('.square').css('height', '20px');
+		$('.square').css('width', '20px');
+	}
+	else {
+		$('.square').css('height', '35px');
+		$('.square').css('width', '35px');
+	}
+})
+
+// here is where the js will end.
+});
